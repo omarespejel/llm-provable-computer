397ff7f3de700c8f5f69ad97ac3f3890f002a57b
Omar U. Espejel <espejelomar@gmail.com>
2026-04-20 13:21:39 +0300

    Harden Phase57 opening verifier bindings

--- src/stwo_backend/mod.rs
+++ src/stwo_backend/mod.rs
@@ -416,7 +416,6 @@ pub use recursion::{
     verify_phase56_executable_sumcheck_component_proof,
     verify_phase56_first_layer_executable_sumcheck_claim,
     verify_phase56_first_layer_executable_sumcheck_claim_against_phase54,
-    verify_phase57_first_layer_mle_opening_verifier_claim,
     verify_phase57_first_layer_mle_opening_verifier_claim_against_phase56,
     verify_phase57_mle_opening_verification_receipt, Phase29RecursiveCompressionInputContract,
     Phase31RecursiveCompressionDecodeBoundaryManifest,
